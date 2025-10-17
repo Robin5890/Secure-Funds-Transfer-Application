1c82f712e04ccbbb74d9f372a43cd3b65af6b911
Robin <spearhead589@gmail.com>
2025-10-17 12:37:44 +0300

    transfer form + fixes

--- frontend/sfta-frontend/src/app/components/transfer/transfer.ts
+++ frontend/sfta-frontend/src/app/components/transfer/transfer.ts
@@ -2,9 +2,10 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
 import { CookieService } from 'ngx-cookie-service';
+import { FormControl, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from "@angular/forms";
 @Component({
   selector: 'app-transfer',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './transfer.html',
   styleUrl: './transfer.css'
 })
@@ -16,6 +17,7 @@ export class Transfer implements OnInit{
   username: string = '';
   balance: number = 0;
 
+  transferForm!: FormGroup;
 
   ngOnInit(): void {
     const token = this.cookie.get('jwt');
@@ -31,6 +33,34 @@ export class Transfer implements OnInit{
         console.error('Failed to fetch user data', err);
       }
   });
+
+  this.transferForm = new FormGroup({
+    recipientID: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9]+$')]),
+    Amount: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]+)?$') , Validators.min(1)])
+  })
+
 }
+
+onSubmit() {
+  if (this.transferForm.invalid) {
+    this.transferForm.markAllAsTouched();
+    return;
+  }
+
+  const { recipientID, Amount } = this.transferForm.value;
+  const token = this.cookie.get('jwt');
+
+  this.http.post(`${environment.apiUrl}/transfer`, { recipientID, Amount }, {
+    headers: { Authorization: `Bearer ${token}` },
+    withCredentials: true
+  }).subscribe({
+    next: res => console.log('Transfer successful', res),
+    error: err => console.error('Transfer failed', err)
+  });
+}
+
+
+
+
 
 }
