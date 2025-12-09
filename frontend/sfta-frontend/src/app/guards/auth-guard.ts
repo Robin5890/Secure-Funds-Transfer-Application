a9cc2a646fdc8bec98bef707de8c89ac7d7c5587
Robin <spearhead589@gmail.com>
2025-12-09 19:11:45 +0200

    Update auth-guard.ts

--- frontend/sfta-frontend/src/app/guards/auth-guard.ts
+++ frontend/sfta-frontend/src/app/guards/auth-guard.ts
@@ -2,12 +2,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, of } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const http = inject(HttpClient);
   const router = inject(Router);
 
-  return http.get('http://localhost:8080/getUserData', { withCredentials: true }).pipe(
+  return http.get(`${environment.apiUrl}/getUserData`, { withCredentials: true }).pipe(
     map(() => true),
     catchError(() => {
       router.navigate(['/login']);
