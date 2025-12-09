1aac9d4adf53b3d66e7d6dea94739fd6b745c1da
Robin <spearhead589@gmail.com>
2025-12-09 18:57:49 +0200

    Update login.ts

--- frontend/sfta-frontend/src/app/components/login/login.ts
+++ frontend/sfta-frontend/src/app/components/login/login.ts
@@ -2,79 +2,65 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service'; 
+import { CookieService } from 'ngx-cookie-service';
 import { environment } from '../../../environments/environment.development';
-
-
 
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrls: ['./login.css']
+  styleUrls: ['./login.css'],
 })
-export class Login implements OnInit{
-
+export class Login implements OnInit {
   userForm!: FormGroup;
   showform = false;
   errorMessage = '';
   feedBackExists = false;
 
- http = inject(HttpClient);
- router = inject(Router); 
- cookieService = inject(CookieService);
-    
+  http = inject(HttpClient);
+  router = inject(Router);
+  cookieService = inject(CookieService);
 
- ngOnInit(): void {
-
-  const token = this.cookieService.get('jwt');
-
-  if(token){
-    this.router.navigate(["/transfer"]);
-    return;
-  }
+  ngOnInit(): void {
+    this.http.get(`${environment.apiUrl}/getUserData`, { withCredentials: true }).subscribe({
+      next: () => this.router.navigate(['/transfer']), // already logged in
+      error: (err) => {
+        if (err.status !== 401) {
+          console.error('Error checking login status', err);
+        }
+      },
+    });
     this.showform = true;
     this.userForm = new FormGroup({
-          username: new FormControl('', [Validators.required]),
-          password: new FormControl('', [Validators.required])
-        })
-     }
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
 
-  onSubmit(){
-            
-  if (this.userForm.valid) {
-      this.http.post<any>(`${environment.apiUrl}/login`, this.userForm.value, { withCredentials: true })
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.http
+        .post<any>(`${environment.apiUrl}/login`, this.userForm.value, { withCredentials: true })
         .subscribe({
-          next:(data) => {
+          next: (data) => {
             this.errorMessage = data.message || 'Login successful';
             this.feedBackExists = true;
             console.log(data.message);
-            if(data.token){
-              this.cookieService.set('jwt', data.token,{
-                expires: 1,
-                path: '/',
-                secure: true,
-                sameSite: 'Strict'
-              });
-              this.router.navigate(['transfer']);
-            }
+            this.router.navigate(['transfer']);
           },
           error: (err) => {
-            if(err.status === 401){
+            if (err.status === 401) {
               this.errorMessage = err.error.message || 'Invalid username or password';
-              
-            }else{
+            } else {
               this.errorMessage = 'An error occured.';
             }
             this.feedBackExists = true;
             console.error('Login failed', err);
-            
-          }
+          },
         });
-    }else{
+    } else {
       this.errorMessage = 'Please fill out all the fields';
       this.feedBackExists = true;
     }
   }
-
 }
